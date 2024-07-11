fde2f19daee02d096c7c355f44e21f3020d90943
Andree Requejo <andree09requejo@gmail.com>
2024-07-11 00:28:24 -0500

    configs

--- producto/views.py
+++ producto/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def HomeView(request):
+    return HttpResponse("El rabazo de Angie chavez aguanto tres golpes, y aprobo calidad")
