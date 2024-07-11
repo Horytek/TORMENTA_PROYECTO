93ce981714bc6d114956ee4714c0d75cfc97c61a
JuanChiscol <chiscolpatazcajuandavid@gmail.com>
2024-07-11 09:11:35 -0500

    modified_sidebaranddashboard_add_registroventa

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -4,12 +4,12 @@ import { Routes, Route } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
 import Navbar from '../Navbar/Navbar';
 import Inicio from '@/pages/Inicio/Inicio';
-import Ventas from '@/pages/Ventas/Ventas';
+import Ventas from '@/pages/Ventas/Venta/Ventas';
 import Empleados from '@/pages/Empleados';
 import Productos from '@/pages/Productos';
 import Almacen from '@/pages/Almacén';
 import Configuracion from '@/pages/Configuración';
-
+import Registro_venta from '@/pages/Ventas/Registro_Venta/Registro_venta';
 
 function Dashboard() {
   return (
@@ -27,6 +27,8 @@ function Dashboard() {
             <Route path="/inicio" element={<Inicio />} />
             {/* Ruta para la página de ventas */}
             <Route path="/ventas" element={<Ventas />} />
+            {/* Ruta para la página de configuración */}
+            <Route path="/ventas/registro_venta" element={<Registro_venta />} />
             {/* Ruta para la página de empleados */}
             <Route path="/empleados" element={<Empleados />} />
             {/* Ruta para la página de productos */}
