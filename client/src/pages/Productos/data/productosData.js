e8abca5595abd54b7764ce5177e8efa53a2333ae
Andree Requejo <andree09requejo@gmail.com>
2024-07-11 17:12:00 -0500

    componentProduct60%

---
+++ client/src/pages/Productos/data/productosData.js
@@ -0,0 +1,95 @@
+// src/data/productosData.js
+const productosData = [
+  {
+    descripcion: 'Camisa de Algodón',
+    linea: 'Producto',
+    subLinea: 'Camisas',
+    unidadMedida: 'Unidad',
+    precio: 25.99,
+    codBarras: '-',
+    estado: 'Activo'
+  },
+  {
+    descripcion: 'Pantalón de Mezclilla',
+    linea: 'Producto',
+    subLinea: 'Pantalones',
+    unidadMedida: 'Unidad',
+    precio: 45.50,
+    codBarras: '-',
+    estado: 'Activo'
+  },
+  {
+    descripcion: 'Materia Prima Algodón',
+    linea: 'Materia Prima',
+    subLinea: 'Materia Prima',
+    unidadMedida: 'Kilogramo',
+    precio: 10.00,
+    codBarras: '-',
+    estado: 'Inactivo'
+  },
+  {
+    descripcion: 'Polos de Algodón',
+    linea: 'Producto',
+    subLinea: 'Polos',
+    unidadMedida: 'Unidad',
+    precio: 20.00,
+    codBarras: '-',
+    estado: 'Activo'
+  },
+  {
+    descripcion: 'Fibras Sintéticas',
+    linea: 'Materia Prima',
+    subLinea: 'Materia Prima',
+    unidadMedida: 'Kilogramo',
+    precio: 12.00,
+    codBarras: '-',
+    estado: 'Activo'
+  },
+  {
+    descripcion: 'Vestido de Noche',
+    linea: 'Producto',
+    subLinea: 'Vestidos',
+    unidadMedida: 'Unidad',
+    precio: 60.00,
+    codBarras: '-',
+    estado: 'Activo'
+  },
+  {
+    descripcion: 'Hilo de Algodón',
+    linea: 'Materia Prima',
+    subLinea: 'Materia Prima',
+    unidadMedida: 'Carrete',
+    precio: 15.00,
+    codBarras: '-',
+    estado: 'Inactivo'
+  },
+  {
+    descripcion: 'Chompa de Lana',
+    linea: 'Producto',
+    subLinea: 'Chompas',
+    unidadMedida: 'Unidad',
+    precio: 35.00,
+    codBarras: '-',
+    estado: 'Activo'
+  },
+  {
+    descripcion: 'Tela de Poliéster',
+    linea: 'Materia Prima',
+    subLinea: 'Materia Prima',
+    unidadMedida: 'Metro',
+    precio: 8.00,
+    codBarras: '-',
+    estado: 'Activo'
+  },
+  {
+    descripcion: 'Chaqueta de Cuero',
+    linea: 'Producto',
+    subLinea: 'Chaquetas',
+    unidadMedida: 'Unidad',
+    precio: 120.00,
+    codBarras: '-',
+    estado: 'Activo'
+  },
+];
+
+export default productosData;
