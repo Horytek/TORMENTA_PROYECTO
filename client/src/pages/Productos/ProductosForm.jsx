c9c2a7a9b9e49b9d919fd903080571827c96c84f
Andree Requejo <andree09requejo@gmail.com>
2024-07-12 03:03:28 -0500

    productComplete

--- client/src/pages/Productos/ProductosForm.jsx
+++ client/src/pages/Productos/ProductosForm.jsx
@@ -1,9 +1,54 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { IoMdClose } from "react-icons/io";
+import { FaRegPlusSquare } from "react-icons/fa";
 import './ProductosForm.css';
 import { ButtonSave, ButtonClose } from '@/components/Buttons/Buttons';
+import { ModalMarca } from './Modal Form/ModalMarca';
+import { ModalLinea } from './Modal Form/ModalLinea';
+import { ModalSublinea } from './Modal Form/ModalSubLinea';
+import { ModalUndMed } from './Modal Form/ModalUndMed';
+
 
 const ProductosForm = ({ modalTitle, onClose }) => {
+
+    const [isModalOpenMarca, setIsModalOpenMarca] = useState(false);
+    const [isModalOpenLinea, setIsModalOpenLinea] = useState(false);
+    const [isModalOpenSubLinea, setIsModalOpenSubLinea] = useState(false);
+    const [isModalOpenUndMed, setIsModalOpenUndMed] = useState(false);
+
+    // Logica Modal Marca
+    const openModalMarca = () => {
+      setIsModalOpenMarca(true);
+    };
+    const closeModalMarca = () => {
+      setIsModalOpenMarca(false);
+    };
+
+    // Logica Modal Linea
+    const openModalLinea = () => {
+      setIsModalOpenLinea(true);
+    };
+    const closeModalLinea = () => {
+      setIsModalOpenLinea(false);
+    };
+
+    // Logica Modal Sublinea
+    const openModalSubLinea = () => {
+      setIsModalOpenSubLinea(true);
+    };
+    const closeModalSubLinea = () => {
+      setIsModalOpenSubLinea(false);
+    };
+
+    // Logica Modal UnidaMedida
+    const openModalUndMed = () => {
+      setIsModalOpenUndMed(true);
+    };
+    const closeModalUndMed = () => {
+      setIsModalOpenUndMed(false);
+    };
+
   return (
     <div className="modal-overlay">
       <div className="modal">
@@ -26,15 +71,22 @@ const ProductosForm = ({ modalTitle, onClose }) => {
             <div className='grid grid-cols-2 gap-6'>
               <div className='w-full relative group mb-5 text-start'>
                 <label htmlFor="linea" className='text-sm font-bold text-black'>Línea:</label>
-                <select id='linea' className='w-full text-sm bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-2'>
-                  <option>Seleccione...</option>
-                </select>
+                <div className='flex justify-center items-center gap-2'>
+                  <select id='linea' className='w-full text-sm bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-2'>
+                    <option>Seleccione...</option>
+                  </select>
+                  <FaRegPlusSquare className='text-2xl cursor-pointer text-gray-500' onClick={openModalLinea} />
+                </div>
+                
               </div>
               <div className='w-full relative group mb-5 text-start'>
                 <label htmlFor="Sub-Línea" className='text-sm font-bold text-black'>Sub-Línea:</label>
-                <select id='sublinea' className='w-full text-sm bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-2'>
-                  <option>Seleccione...</option>
-                </select>
+                <div className='flex justify-center items-center gap-2'>
+                  <select id='sublinea' className='w-full text-sm bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-2'>
+                    <option>Seleccione...</option>
+                  </select>
+                  <FaRegPlusSquare className='text-2xl cursor-pointer text-gray-500' onClick={openModalSubLinea} />
+                </div>
               </div>
             </div>
 
@@ -64,9 +116,12 @@ const ProductosForm = ({ modalTitle, onClose }) => {
               </div>
               <div className='w-full relative group mb-5 text-start'>
                 <label htmlFor="Sub-Línea" className='text-sm font-bold text-black'>Marca:</label>
-                <select id='marca' className='w-full text-sm bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-2'>
-                  <option>Seleccione...</option>
-                </select>
+                <div className='flex justify-center items-center gap-2'>
+                  <select id='marca' className='w-full text-sm bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-2'>
+                    <option>Seleccione...</option>
+                  </select>
+                  <FaRegPlusSquare className='text-2xl cursor-pointer text-gray-500' onClick={openModalMarca} />
+                </div>
               </div>
             </div>
 
@@ -75,9 +130,12 @@ const ProductosForm = ({ modalTitle, onClose }) => {
             <div className='grid grid-cols-2 gap-6'>
               <div className='w-full relative group mb-5 text-start'>
                 <label htmlFor="unidadMedida" className='text-sm font-bold text-black'>Und. Medida::</label>
-                <select id='unidadMedida' className='w-full text-sm bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-2'>
-                  <option>Seleccione...</option>
-                </select>
+                <div className='flex justify-center items-center gap-2'>
+                  <select id='unidadMedida' className='w-full text-sm bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-2'>
+                    <option>Seleccione...</option>
+                  </select>
+                  <FaRegPlusSquare className='text-2xl cursor-pointer text-gray-500' onClick={openModalUndMed} />
+                </div>
               </div>
               <div className='w-full relative group mb-5 text-start'>
                 <label htmlFor="estado" className='text-sm font-bold text-black'>Estado:</label>
@@ -95,6 +153,27 @@ const ProductosForm = ({ modalTitle, onClose }) => {
             </div>
         </div>
       </div>
+
+      {/* Modal de Nueva Marca */}
+      {isModalOpenMarca && (
+        <ModalMarca modalTitle={'Marca'} closeModel={closeModalMarca} />
+      )}
+
+      {/* Modal de Nueva Linea */}
+      {isModalOpenLinea && (
+        <ModalLinea modalTitle={'Linea'} closeModel={closeModalLinea} />
+      )}
+
+      {/* Modal de Nueva SubLinea */}
+      {isModalOpenSubLinea && (
+        <ModalSublinea modalTitle={'Sub-Línea'} closeModel={closeModalSubLinea} />
+      )}
+
+      {/* Modal de Nueva Unidad Medida */}
+      {isModalOpenUndMed && (
+        <ModalUndMed modalTitle={'Unidad de Medida'} closeModel={closeModalUndMed} />
+      )}
+
     </div>
   );
 };
