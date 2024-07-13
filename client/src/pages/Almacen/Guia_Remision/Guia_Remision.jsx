f349d627db4e4bcae420c56e3f2656a0d7293553
adriannportok <pink21guy@gmail.com>
2024-07-13 04:34:09 -0500

    GUIA DE REMISION 99%

--- client/src/pages/Almacen/Guia_Remision/Guia_Remision.jsx
+++ client/src/pages/Almacen/Guia_Remision/Guia_Remision.jsx
@@ -1,60 +1,39 @@
 import { useState } from 'react';
 import './Guia_Remision.css';
 import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
-import { MdAddCircleOutline, MdEdit } from 'react-icons/md';
-
+import { MdAddCircleOutline } from 'react-icons/md';
 import Pagination from '@/components/Pagination/Pagination';
 import TablaGuias from './ComponentsGuias/GuiasTable';
 import FiltrosGuias from './ComponentsGuias/FiltrosGuias';
-import OptionsModal from './ComponentsGuias/Modals/OptionsModal';
-import ConfirmationModal from './ComponentsGuias/Modals/ConfirmationModal';
 import { Link } from 'react-router-dom';
 import useGuiasData from '../data/guiasdata';
+import ConfirmationModal from '@/components/Modals/ConfirmationModal';
 
 const Guias = () => {
-  /* const renderActions = (row) => (
-    <div className="flex justify-center items-center">
-      <button className="px-2 py-1 text-yellow-400 text-xl" onClick={() => openModal('Editar Producto')}>
-        <MdEdit />
-      </button>
-      <button className="px-2 py-1 text-red-500" onClick={() => handleOpenConfirmationModal(row)}>
-        <FaTrash />
-      </button>
-    </div>
-  ); */
-  // Estado para manejar la lista de guias
-  const { guias, removeGuia  } = useGuiasData();
+  const { guias, removeGuia } = useGuiasData();
 
-  // Estado para el manejo del modal y opciones de eliminación
+  // Estado para el manejo del modal de confirmación
+  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
   const [selectedRowId, setSelectedRowId] = useState(null);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [deleteOptionSelected, setDeleteOptionSelected] = useState(false);
-  const [confirmDeleteModalOpen, setConfirmDeleteModalOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const totalPages = 5; // Número total de páginas
 
-  // Funciones para abrir y cerrar el modal de opciones
-  const openModal = (id) => {
-    setSelectedRowId(id);
-    setModalOpen(true);
+  // Función para manejar la acción de abrir el modal de confirmación
+  const handleOpenConfirmationModal = (rowId) => {
+    setSelectedRowId(rowId);
+    setIsConfirmationModalOpen(true);
   };
 
-  const closeModal = () => {
+  // Función para manejar la acción de cerrar el modal de confirmación
+  const handleCloseConfirmationModal = () => {
+    setIsConfirmationModalOpen(false);
     setSelectedRowId(null);
-    setModalOpen(false);
-    setDeleteOptionSelected(false);
   };
 
-  // Función para alternar la opción de eliminar guia
-  const toggleDeleteDetalleOption = () => {
-    setDeleteOptionSelected(!deleteOptionSelected);
-  };
-
-  // Función para eliminar una guia
-  const handleDeleteGuia = () => {
+  // Función para manejar la acción de confirmar eliminar
+  const handleConfirmDelete = () => {
     removeGuia(selectedRowId);
-    closeModal();
-    setConfirmDeleteModalOpen(false);
+    handleCloseConfirmationModal();
   };
 
   // Función para cambiar de página en la paginación
@@ -64,52 +43,26 @@ const Guias = () => {
 
   return (
     <div>
-      {/* Componente de migas de pan */}
-      <Breadcrumb paths={[{ name: 'Inicio', href: '/inicio' }, { name: 'Almacen', href: '/almacen' }, { name: 'Guias de Remision', href: '/guiasremision' }]} />
+      <Breadcrumb paths={[{ name: 'Inicio', href: '/inicio' }, { name: 'Almacen', href: '/almacen' }, { name: 'Guias de Remision', href: '/almacen/guia_remision' }]} />
 
       <hr className="mb-4" />
-      {/* Encabezado principal */}
       <div className="flex justify-between mt-5 mb-4">
         <h1 className="text-xl font-bold" style={{ fontSize: '36px' }}>
           Guias de Remision
-        </h1>             
+        </h1>
         <Link to="/almacen/guia_remision/registro_guia" className="btn btn-nueva-guia mr-0">
           <MdAddCircleOutline className="inline-block mr-2" style={{ fontSize: '25px' }} />
           Nueva guia
         </Link>
-        
       </div>
 
-      {/* Componente de filtros */}
       <FiltrosGuias />
 
-      {/* Componente de tabla de guias */}
       <TablaGuias
         guias={guias}
-        modalOpen={modalOpen}
-        deleteOptionSelected={deleteOptionSelected}
-        openModal={openModal}
-        /* currentPage={currentPage} */
+        handleOpenConfirmationModal={handleOpenConfirmationModal}
       />
 
-      {/* Modal para opciones */}
-      <OptionsModal
-        modalOpen={modalOpen}
-        toggleDeleteDetalleOption={toggleDeleteDetalleOption}
-        closeModal={closeModal}
-        setConfirmDeleteModalOpen={setConfirmDeleteModalOpen}
-        deleteOptionSelected={deleteOptionSelected}
-      />
-
-      {/* Modal de confirmación de eliminación */}
-      <ConfirmationModal
-        confirmDeleteModalOpen={confirmDeleteModalOpen}
-        handleDeleteGuia={handleDeleteGuia}
-        closeModal={closeModal}
-        setConfirmDeleteModalOpen={setConfirmDeleteModalOpen}
-      />
-
-      {/* Contenedor para paginación */}
       <div className="flex justify-between mt-4">
         <div className="flex">
           <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
@@ -120,6 +73,14 @@ const Guias = () => {
           <option>20</option>
         </select>
       </div>
+
+      {isConfirmationModalOpen && (
+        <ConfirmationModal
+          message={`¿Estás seguro que deseas eliminar esta guía?`}
+          onClose={handleCloseConfirmationModal}
+          onConfirm={handleConfirmDelete}
+        />
+      )}
     </div>
   );
 };
--- client/src/pages/Almacen/Guia_Remision/Registro_Guia/ClienteForm.jsx
+++ client/src/pages/Almacen/Guia_Remision/Registro_Guia/ClienteForm.jsx
@@ -5,65 +5,106 @@ import './ModalGuias.css';
 import { ButtonSave, ButtonClose } from '@/components/Buttons/Buttons';
 
 function ClienteForm({ modalTitle, onClose }) {
-  const [tab, setTab] = useState('registro'); // State to manage tabs
+  const [tab, setTab] = useState('registro');
 
   return (
-    <div className="modal-overlay">
-      <div className="modal">
-        <div className='content-modal'>
-          <div className="modal-header">
-            <h3 className="modal-title">{modalTitle}</h3>
-            <button className="modal-close" onClick={onClose}>
+    <div className="modal3-overlay">
+      <div className="modal3">
+        <div className='content-modal3'>
+          <div className="modal3-header">
+            <h2 className="modal3-title">{modalTitle}</h2>
+            <button className="close-button" onClick={onClose}>
               <IoMdClose className='text-3xl'/>
             </button>
           </div>
-          <div className='modal-body'>
+          <div className="modal-bodywa">
             <div className="tabs">
-              <button className={tab === 'registro' ? 'active' : ''} onClick={() => setTab('registro')}>Registro</button>
-              <button className={tab === 'otros' ? 'active' : ''} onClick={() => setTab('otros')}>Otros datos</button>
+              <button 
+                className={`p-4 ${tab === 'registro' ? 'active' : ''}`}
+                onClick={() => setTab('registro')}
+              >
+                Registro
+              </button>
+              <button 
+                className={`p-4 ${tab === 'otros' ? 'active' : ''}`}
+                onClick={() => setTab('otros')}
+              >
+                Otros datos
+              </button>
             </div>
-            {tab === 'registro' && (
-              <div className='modal-content'>
-                <label>RUC/DNI:</label>
-                <input type="text" placeholder="12345678" />
-                <label>Nombre:</label>
-                <input type="text" placeholder="Jorge Saldarriaga Vignolo" />
-                <label>Rz. Comercial:</label>
-                <input type="text" />
-                <label>Teléfono:</label>
-                <input type="text" />
-                <label>Pers. Contacto:</label>
-                <input type="text" />
-                <label>Dirección:</label>
-                <input type="text" />
-                <label>Email:</label>
-                <input type="email" />
-                <label>Cat. Sunat:</label>
-                <select>
-                  <option>Seleccione</option>
-                </select>
-                <label>Status Sunat:</label>
-                <select>
-                  <option>Seleccione</option>
-                </select>
+            <form>
+              {tab === 'registro' && (
+                <div className='modal2-content'>
+                  <div className="form-row">
+                    <div className="form-group">
+                      <label className='text-sm font-bold text-black' htmlFor="ruc-dni">RUC/DNI:</label>
+                      <input className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5 wider2-input' type="text" id="ruc-dni" />
+                    </div>
+                    <div className="items-center justify-center pt-5">
+                      <button className="sunat-button rounded-lg border p-2.5">SUNAT</button>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label className='text-sm font-bold text-black' htmlFor="nombre">Nombre:</label>
+                    <input className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5' type="text" id="nombre" />
+                  </div>
+                  <div className="form-group">
+                    <label className='text-sm font-bold text-black' htmlFor="rz-comercial">Rz. Comercial:</label>
+                    <input className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5' type="text" id="rz-comercial" />
+                  </div>
+                  <div className="form-group">
+                    <label className='text-sm font-bold text-black' htmlFor="telefono">Teléfono:</label>
+                    <input className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5' type="text" id="telefono" />
+                  </div>
+                  <div className="form-group">
+                    <label className='text-sm font-bold text-black' htmlFor="contacto">Pers. Contacto:</label>
+                    <input className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5' type="text" id="contacto" />
+                  </div>
+                  <div className="form-group">
+                    <label className='text-sm font-bold text-black' htmlFor="direccion">Dirección:</label>
+                    <input className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5' type="text" id="direccion" />
+                  </div>
+                  <div className="form-group">
+                    <label className='text-sm font-bold text-black' htmlFor="email">Email:</label>
+                    <input className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5' type="email" id="email" />
+                  </div>
+                  <div className="form-row">
+                    <div className="form-group">
+                      <label className='text-sm font-bold text-black' htmlFor="cat-sunat">Cat. Sunat:</label>
+                      <select id="cat-sunat" className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5'>
+                        <option>Seleccione</option>
+                      </select>
+                    </div>
+                    <div className="form-group">
+                      <label className='text-sm font-bold text-black' htmlFor="status-sunat">Status Sunat:</label>
+                      <select id="status-sunat" className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5'>
+                        <option>Seleccione</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {tab === 'otros' && (
+                <div className='modal2-content'>
+                  <div className="form-group">
+                    <label className='text-sm font-bold text-black' htmlFor="categoria">Categoria:</label>
+                    <select id="categoria" className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5'>
+                      <option>Seleccione</option>
+                    </select>
+                  </div>
+                  <div className="form-group">
+                    <label className='text-sm font-bold text-black' htmlFor="vendedor">Vendedor:</label>
+                    <select id="vendedor" className='w-full bg-gray-50 border-gray-300 text-gray-900 rounded-lg border p-1.5'>
+                      <option>Seleccione</option>
+                    </select>
+                  </div>
+                </div>
+              )}
+              <div className="modal-buttons">
+                <ButtonClose onClick={onClose} />
+                <ButtonSave />
               </div>
-            )}
-            {tab === 'otros' && (
-              <div className='modal-content'>
-                <label>Categoria:</label>
-                <select>
-                  <option>Seleccione</option>
-                </select>
-                <label>Vendedor:</label>
-                <select>
-                  <option>Seleccione</option>
-                </select>
-              </div>
-            )}
-          </div>
-          <div className='modal-buttons'>
-            <ButtonClose onClick={onClose}/>
-            <ButtonSave/>
+            </form>
           </div>
         </div>
       </div>
