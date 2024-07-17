3e6dbab9ecdc56fd4afaf60bce394490fdcb33ba
davist <bustamante777a@gmail.com>
2024-07-17 02:39:26 -0500

    prueba_lista_ventas

--- client/src/pages/Productos/data/productosData.js
+++ client/src/pages/Productos/data/productosData.js
@@ -12,7 +12,7 @@ const getProductosRequest = async () => {
         undm: item.undm,
         precio: parseFloat(item.precio), // Convertir el precio a número flotante
         cod_barras: item.cod_barras || '-', // Manejar el caso donde cod_barras pueda ser null
-        estado: item.estado === 0 ? 'Inactivo' : 'Activo' // Convertir estado a texto según convención de productosData
+        estado: parseInt(item.estado) === 0 ? 'Inactivo' : 'Activo' // Convertir estado a texto según convención de productosData
       }));
       
       return productos; // Devuelve el array de productos transformados
