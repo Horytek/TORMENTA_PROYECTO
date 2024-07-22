5f3059b07ed92770f9a00b483fdefc2f2ff2d168
Andree Requejo <andree09requejo@gmail.com>
2024-07-22 00:37:42 -0500

    config

--- client/src/utils/api.utils.js
+++
@@ -1,9 +0,0 @@
-export async function handleApiResponse (apiFunction) {
-  try {
-    const response = await apiFunction();
-    return response.data;
-  } catch (error) {
-    console.error("API Error:", error);
-    throw error;
-  }
-}
