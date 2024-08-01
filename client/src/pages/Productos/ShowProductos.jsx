0e731b83e9b456ff30789b2d16dd2bf2b70d4f4f
Andree Requejo <andree09requejo@gmail.com>
2024-08-01 01:52:04 -0500

    edit-design

--- client/src/pages/Productos/ShowProductos.jsx
+++ client/src/pages/Productos/ShowProductos.jsx
@@ -12,7 +12,7 @@ export function ShowProductos() {
     // Estados de listado de productos
     const [productos, setProductos] = useState([]);
     const [currentPage, setCurrentPage] = useState(1); // Página inicial
-    const productosPerPage = 15;
+    const productosPerPage = 4;
 
     useEffect(() => {
         getProducts();
@@ -63,61 +63,63 @@ export function ShowProductos() {
     };
 
     return (
-        <div className="overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm table-auto divide-gray-200 rounded-lg">
-                <thead className="bg-gray-50">
-                    <tr>
-                        <th className="px-6 py-3 w-1/3 text-xs font-bold text-gray-500 uppercase text-center">DESCRIPCIÓN</th>
-                        <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">LÍNEA</th>
-                        <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">SUB-LÍNEA</th>
-                        <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">UND. MED.</th>
-                        <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">PRECIO (S/.)</th>
-                        <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">COD. BARRAS</th>
-                        <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">ESTADO</th>
-                        <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">ACCIONES</th>
-                    </tr>
-                </thead>
-                <tbody className="bg-white divide-gray-200">
-                    { currentProductos.map((producto) => (
-                        <tr className='hover:bg-gray-100' key={producto.id_producto} data-product={producto.id_producto}>
-                            <td className='py-2 px-2 max-w-xs whitespace-nowrap'>{producto.descripcion}</td>
-                            <td className='py-2 text-center'>{producto.nom_marca}</td>
-                            <td className='py-2 text-center'>{producto.nom_subcat}</td>
-                            <td className='py-2 text-center'>{producto.undm}</td>
-                            <td className='py-2 text-center'>{producto.precio}</td>
-                            <td className='py-2 text-center'>
-                                {producto.cod_barras === '-' ? '-' : 
-                                  <div className="flex justify-center items-center">
-                                    <Barcode
-                                      className="bg-transparent"
-                                      value={producto.cod_barras}
-                                    />
-                                  </div>
-                                } 
-                            </td>
-                            <td className='py-2 text-center'>
-                                <span className={
-                                    producto.estado_producto === 'Inactivo'
-                                        ? "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-medium font-normal bg-red-100 text-red-600" 
-                                        : "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-medium font-normal bg-green-200 text-green-700"
-                                }>
-                                    {producto.estado_producto}
-                                </span>
-                            </td>
-                            <td className='py-4 text-center'>
-                                <div className="flex justify-center items-center">
-                                    <button className="px-2 py-1 text-yellow-400 text-xl" onClick={handleModalEdit}>
-                                        <MdEdit />
-                                    </button>
-                                    <button className="px-2 py-1 text-red-500" onClick={() => handleOpenConfirmationModal(producto.descripcion, producto.id_producto)}>
-                                        <FaTrash />
-                                    </button>
-                                </div>
-                            </td>
+        <div>
+            <div className="overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm table-auto divide-gray-200 rounded-lg">
+                    <thead className="bg-gray-50">
+                        <tr>
+                            <th className="px-6 py-3 w-1/3 text-xs font-bold text-gray-500 uppercase text-center">DESCRIPCIÓN</th>
+                            <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">LÍNEA</th>
+                            <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">SUB-LÍNEA</th>
+                            <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">UND. MED.</th>
+                            <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">PRECIO (S/.)</th>
+                            <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">COD. BARRAS</th>
+                            <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">ESTADO</th>
+                            <th className="px-6 py-3 text-xs font-bold text-gray-500 uppercase text-center">ACCIONES</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="bg-white divide-gray-200">
+                        { currentProductos.map((producto) => (
+                            <tr className='hover:bg-gray-100' key={producto.id_producto} data-product={producto.id_producto}>
+                                <td className='py-2 px-2 max-w-xs whitespace-nowrap'>{producto.descripcion}</td>
+                                <td className='py-2 text-center'>{producto.nom_marca}</td>
+                                <td className='py-2 text-center'>{producto.nom_subcat}</td>
+                                <td className='py-2 text-center'>{producto.undm}</td>
+                                <td className='py-2 text-center'>{producto.precio}</td>
+                                <td className='py-2 text-center'>
+                                    {producto.cod_barras === '-' ? '-' : 
+                                      <div className="flex justify-center items-center">
+                                        <Barcode
+                                          className="bg-transparent"
+                                          value={producto.cod_barras}
+                                        />
+                                      </div>
+                                    } 
+                                </td>
+                                <td className='py-2 text-center'>
+                                    <span className={
+                                        producto.estado_producto === 'Inactivo'
+                                            ? "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-medium font-normal bg-red-100 text-red-600" 
+                                            : "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-medium font-normal bg-green-200 text-green-700"
+                                    }>
+                                        {producto.estado_producto}
+                                    </span>
+                                </td>
+                                <td className='py-4 text-center'>
+                                    <div className="flex justify-center items-center">
+                                        <button className="px-2 py-1 text-yellow-400 text-xl" onClick={handleModalEdit}>
+                                            <MdEdit />
+                                        </button>
+                                        <button className="px-2 py-1 text-red-500" onClick={() => handleOpenConfirmationModal(producto.descripcion, producto.id_producto)}>
+                                            <FaTrash />
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
 
             {/* Paginación */}
             <div className="flex justify-end mt-4">
@@ -129,7 +131,7 @@ export function ShowProductos() {
                     />
                 </div>
             </div>
-
+    
             {/* Modal de Eliminar Producto */}
             {isConfirmationModalOpen && (
                 <ConfirmationModal
@@ -138,7 +140,7 @@ export function ShowProductos() {
                     onConfirm={handleConfirmDelete}
                 />
             )}
-
+    
             {/* Modal de Editar Producto */}
             {activeEdit && (
                 <ProductosForm modalTitle={'Editar Producto'} onClose={handleModalEdit} />
