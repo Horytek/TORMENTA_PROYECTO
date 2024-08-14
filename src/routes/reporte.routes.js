ce02a77cf3149d1d86335a1eb4ee19fbaafac3d4
ryushade <77291154@usat.pe>
2024-08-14 00:47:59 -0500

    yacasi bro

--- src/routes/reporte.routes.js
+++ src/routes/reporte.routes.js
@@ -4,7 +4,7 @@ import { methods as reporteController } from "./../controllers/reporte.controlle
 const router = Router();
 
 router.get("/ganancias", reporteController.getTotalSalesRevenue);
-router.post("/productos_vendidos", reporteController.getTotalProductosVendidos);
+router.get("/productos_vendidos", reporteController.getTotalProductosVendidos);
 router.get("/producto_top", reporteController.getProductoMasVendido);
 router.get("/cantidad_por_producto", reporteController.getCantidadVentasPorProducto);
 router.get("/cantidad_por_subcategoria", reporteController.getCantidadVentasPorSubcategoria);
