6cb95b85324a574322bb26a967cd13b9edbaab1c
adriannportok <pink21guy@gmail.com>
2024-08-14 19:28:43 -0500

    FALTA FILTRAR CLIENTES Y SUCURSAL, PDF, API SUNAT

--- client/src/pages/Almacen/data/insert_guiaremision.js
+++ client/src/pages/Almacen/data/insert_guiaremision.js
@@ -1,28 +1,9 @@
 import axios from 'axios';
 
 const insertGuiaRemisionAndDetalle = async (data) => {
-  const {
-    id_sucursal, id_ubigeo_o,id_ubigeo_d, id_destinatario, id_transportista, glosa, dir_partida, dir_destino, observacion, f_generacion, h_generacion, producto, num_comprobante, cantidad,
-  } = data;
 
   try {
-    const response = await axios.post('http://localhost:4000/api/guia_remision/nuevaguia', {
-      id_sucursal,
-      id_ubigeo_o,
-      id_ubigeo_d,
-      id_destinatario,
-      id_transportista,
-      glosa,
-      dir_partida,
-      dir_destino,
-      observacion,
-      f_generacion,
-      h_generacion,
-      producto,
-      num_comprobante,
-      cantidad
-    });
-
+    const response = await axios.post('http://localhost:4000/api/guia_remision/nuevaguia', data);
     if (response.data.code === 1) {
       console.log('Guía de remisión y detalles insertados correctamente');
       return { success: true, message: 'Guía de remisión y detalles insertados correctamente' };
