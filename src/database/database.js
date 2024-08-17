aba62910626a3d93c751e3b518f3ae911a5c315c
Andree Requejo <andree09requejo@gmail.com>
2024-08-17 12:28:43 -0500

    config changes

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,20 @@
 import mysql from "mysql2/promise";
+import fs from 'fs';
+import path from 'path';
 import { HOST, DATABASE, USER, PASSWORD, PORT_DB } from "../config.js";
+
+const sslOptions = {
+    ca: fs.readFileSync(path.resolve(__dirname, '../ca.pem')),
+    rejectUnauthorized: false
+};
 
 const connection = mysql.createConnection({
     host: HOST,
     database: DATABASE,
     user: USER,
     password: PASSWORD,
-    port: PORT_DB
+    port: PORT_DB,
+    ssl: sslOptions
 });
 
 const getConnection = () => {
