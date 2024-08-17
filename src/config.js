0099838635d58342f8da137c3dbd905029f158c8
davist <bustamante777a@gmail.com>
2024-08-17 05:20:23 -0500

    mas_tarde_hacemos_el_despliegue

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ import { config } from "dotenv";
 config();
 
 export const HOST = process.env.HOST || "";
-export const PORT = process.env.PORT || "";
 export const DATABASE = process.env.DATABASE || "";
 export const USER = process.env.USER || "";
 export const PASSWORD = process.env.PASSWORD || "";
--- src/database/database.js
+++ src/database/database.js
@@ -1,26 +1,17 @@
-import mysql from 'mysql2/promise';
-import { HOST, PORT, DATABASE, USER, PASSWORD, SSL_CA_CONTENT } from '../config.js';
+import mysql from "mysql2/promise";
+import { HOST, DATABASE, USER, PASSWORD } from "../config.js";
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
     host: HOST,
-    port: PORT,
     database: DATABASE,
     user: USER,
-    password: PASSWORD,
-    ssl: {
-        ca: SSL_CA_CONTENT // Usa el contenido del certificado directamente
-    }
+    password: PASSWORD
 });
 
-const getConnection = async () => {
-    try {
-        const conn = await connection.getConnection();
-        console.log("Connected to the database.");
-        return conn;
-    } catch (err) {
-        console.error("Error connecting to the database: ", err);
-        throw err;
-    }
+const getConnection = () => {
+    return connection;
 };
 
-export default getConnection;
+module.exports = {
+    getConnection
+};
