d11eecb772ca73c57657b053d0b95a1f9b619cc5
Andree Requejo <andree09requejo@gmail.com>
2024-08-17 10:17:54 -0500

    configs and normalizations

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,12 @@
 import mysql from "mysql2/promise";
-import { HOST, DATABASE, USER, PASSWORD, PORT } from "../config.js";
+import { HOST, DATABASE, USER, PASSWORD, PORT_DB } from "../config.js";
 
 const connection = mysql.createConnection({
     host: HOST,
     database: DATABASE,
     user: USER,
     password: PASSWORD,
-    port: PORT,
+    port: PORT_DB
 });
 
 const getConnection = () => {
