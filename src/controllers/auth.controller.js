6fbef24cf019948a23522357202a0950f66c426e
davist <bustamante777a@gmail.com>
2024-08-18 16:12:06 -0500

    veamos_15

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -20,7 +20,7 @@ const login = async (req, res) => {
         
         if (userValid.length > 0) {
             const token = await createAccessToken({ nameUser: user.usuario });
-            res.cookie("token", token);
+            res.cookie("_vercel_jwt", token);
             const userbd = userValid[0]
             res.json({ success: true, data: {
                 id: userbd.id_usuario,
