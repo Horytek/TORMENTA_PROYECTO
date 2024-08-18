4e660bf0a9dfe9ce515c1131ea0dfa57c2e888d0
davist <bustamante777a@gmail.com>
2024-08-18 16:07:38 -0500

    prueba_14

--- client/src/layouts/Login/Login.jsx
+++ client/src/layouts/Login/Login.jsx
@@ -1,6 +1,6 @@
 import './Login.css';
-import { useState } from 'react';
-import { redirect, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import loginImage from '@/assets/img-login.png';
 import AlertModal from '@/components/Modals/AlertModal';
@@ -18,14 +18,15 @@ function Login() {
   // Contexto de autenticación
   const { login, isAuthenticated } = useAuth();
 
-  if  (isAuthenticated) {
-    navigate('/Inicio');
-  } else {
-    redirect('/Login');
-  }
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/Inicio');
+    }
+  }, [isAuthenticated, navigate]);
 
   // Maneja el evento de inicio de sesión
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
     try {
       const user = { usuario, password };
       const response = await login(user);
@@ -40,15 +41,15 @@ function Login() {
       setShowAlert(true);
     }
   };
-  //Comentario
+
   // Renderiza un campo de entrada con o sin opción de mostrar/ocultar contraseña
   const renderInputField = (type, value, setValue, placeholder, label, showToggle = false) => (
-    <div className="input-container relative mb-4">
+    <div className="relative mb-4 input-container">
       <input
         type={type}
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        className="input-field w-full px-4 py-2 rounded-lg focus:outline-none border border-gray-300 focus:border-gray-300 focus:ring-gray-300"
+        className="w-full px-4 py-2 border border-gray-300 rounded-lg input-field focus:outline-none focus:border-gray-300 focus:ring-gray-300"
         placeholder={placeholder}
         autoComplete='current-password'
       />
@@ -57,7 +58,7 @@ function Login() {
       </label>
       {showToggle && (
         <div
-          className="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer"
+          className="absolute transform -translate-y-1/2 cursor-pointer right-4 top-1/2"
           onClick={() => setShowPassword(!showPassword)}
         >
           {showPassword ? <FaEyeSlash /> : <FaEye />}
@@ -67,23 +68,23 @@ function Login() {
   );
 
   return (
-    <form>
+    <form onSubmit={handleLogin}>
       <div className="min-h-screen flex items-center justify-center bg-[#a07ce9]">
         {/* Fondos decorativos */}
-        <div className="bg-circle-top-left absolute top-0 left-0 w-96 h-96 rounded-full z-0"></div>
-        <div className="bg-circle-top-right absolute top-0 right-0 w-40 h-40 rounded-full z-0"></div>
+        <div className="absolute top-0 left-0 z-0 rounded-full bg-circle-top-left w-96 h-96"></div>
+        <div className="absolute top-0 right-0 z-0 w-40 h-40 rounded-full bg-circle-top-right"></div>
   
         {/* Contenedor principal del formulario */}
         <div className="login-container rounded-lg z-10 grid grid-cols-1 lg:grid-cols-2 w-[70vw] h-[70vh]">
           {/* Panel izquierdo (formulario de inicio de sesión) */}
-          <div className="login-form bg-white flex flex-col justify-center p-20">
+          <div className="flex flex-col justify-center p-20 bg-white login-form">
             <h1 className="text-3xl font-bold text-center pb-14">Iniciar Sesión</h1>
   
             {renderInputField("text", usuario, setUsuario, "Tormenta", "Usuario")}
             {renderInputField(showPassword ? "text" : "password", password, setPassword, "*******", "Contraseña", true)}
   
             <button
-              onClick={handleLogin}
+              type="submit"
               className="login-button w-full text-white py-2 rounded focus:outline-none bg-[#00BDD6]"
             >
               Iniciar sesión
