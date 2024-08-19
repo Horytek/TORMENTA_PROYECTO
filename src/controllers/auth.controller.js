414293c3f729a2b4456e4959bf34be02b3c1a894
davist <bustamante777a@gmail.com>
2024-08-19 12:57:43 -0500

    Mishi

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -21,7 +21,7 @@ const login = async (req, res) => {
         if (userValid.length > 0) {
             const token = await createAccessToken({ nameUser: user.usuario });
             res.cookie("token", token, 
-            {sameSite: 'none', secure: true, httpOnly: true, maxAge: 24 * 60 * 60 * 1000, domain: 'bck-omega.vercel.app'});
+            {sameSite: 'none', secure: true, httpOnly: true, maxAge: 24 * 60 * 60 * 1000, domain: '.bck-omega.vercel.app'});
             const userbd = userValid[0]
             res.json({ success: true, data: {
                 id: userbd.id_usuario,
