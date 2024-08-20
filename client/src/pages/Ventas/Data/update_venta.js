684d82593764022b7f43a20ff6c8dcf631276408
davist <bustamante777a@gmail.com>
2024-08-20 00:38:14 -0500

    Finalmente

--- client/src/pages/Ventas/Data/update_venta.js
+++ client/src/pages/Ventas/Data/update_venta.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-import {toast} from "react-hot-toast";
-//import axios from "../../../api/axios";
+//import axios from 'axios';
+//import {toast} from "react-hot-toast";
+import axios from "../../../api/axios";
 
 // Maneja la solicitud de cobro
 export const handleUpdate = async (datosVenta) => {
     try {
         console.log('Datos de venta:', datosVenta);
-        const response = await axios.post('https://bac-orcin.vercel.app/api/ventas/actualizar_venta', {
+        const response = await axios.post('/ventas/actualizar_venta', {
             id_venta: datosVenta.id
           });
 
         if (response.status === 200) {
-            toast.success('Venta eliminada correctamente');
+            //toast.success('Venta eliminada correctamente');
         } else {
             console.error('Error al registrar la venta:', response.data);
         }
