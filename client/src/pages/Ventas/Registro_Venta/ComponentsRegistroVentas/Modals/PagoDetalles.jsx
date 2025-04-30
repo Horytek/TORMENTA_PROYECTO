6866f19d2d2881fc737d5a891e8e4d7f916de90d
Davist Edu <131993237+davistedubustamante@users.noreply.github.com>
2025-04-30 14:56:27 -0500

    Mejora en la disposicion del codigo en ventas

---
+++ client/src/pages/Ventas/Registro_Venta/ComponentsRegistroVentas/Modals/PagoDetalles.jsx
@@ -0,0 +1,192 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Input, Checkbox, Select, SelectItem } from "@nextui-org/react";
+
+const PagoDetalles = ({
+  totalImporte,
+  metodo_pago,
+  setmetodo_pago,
+  disabledKeys1,
+  options,
+  montoRecibido,
+  setMontoRecibido,
+  validateDecimalInput,
+  descuentoActivado,
+  setDescuentoActivado,
+  montoDescuento,
+  setMontoDescuento,
+  cambio,
+  faltante,
+}) => {
+  return (
+    <>
+                        <div className="flex mb-4">
+      {/* Total a pagar */}
+      <Input
+        label="Total a pagar"
+        labelPlacement="outside"
+        value={`S/. ${totalImporte}`}
+        readOnly
+        className="input-c w-40 ml-2"
+        style={{
+          height: "40px",
+          border: "1px solid #ccc",
+          backgroundColor: "#f5f5f5",
+          borderRadius: "8px",
+          padding: "0 12px",
+          fontSize: "16px",
+          color: "#333",
+          outline: "none",
+          boxShadow: "inset 0 0 5px rgba(0, 0, 0, 0.1)",
+          transition: "box-shadow 0.3s ease",
+        }}
+      />
+
+      {/* Método de pago */}
+      <div style={{ marginLeft: "20px" }}>
+        <Select
+          isRequired
+          label="Método de pago"
+          labelPlacement="outside"
+          placeholder="Método de pago"
+          className={"input-c h-10 pr-8"}
+          classNamediv={"flex items-center mt-2"}
+          value={metodo_pago}
+          style={{ width: '13rem' }}
+          onChange={(e) => setmetodo_pago(e.target.value)}
+          containerStyle={{ marginLeft: "5px" }}
+          disabledKeys={disabledKeys1}
+        >
+          {options.map(({ key, value, label }) => (
+            <SelectItem key={key} value={value}>
+              {label}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+      </div>
+      <div className="flex">
+        <Input
+          label="Monto recibido"
+          labelPlacement="outside"
+          placeholder="S/."
+          value={montoRecibido}
+          onChange={(e) => setMontoRecibido(e.target.value)}
+          pattern="[0-9]*[.]?[0-9]{0,2}"
+          onKeyDown={validateDecimalInput}
+          className="input-c w-40 ml-2"
+          style={{
+            height: "40px",
+            border: "1px solid #ddd",
+            borderRadius: "8px",
+            padding: "0 12px",
+            fontSize: "16px",
+            color: "#333",
+            outline: "none",
+            transition: "border-color 0.3s ease, box-shadow 0.3s ease",
+          }}
+        />
+
+        <div className="mb-4 ml-[45px]">
+          <div className="flex items-center mt-2 space-x-3">
+            <Checkbox
+              isSelected={descuentoActivado}
+              onValueChange={setDescuentoActivado}
+              className="mt-1"
+            >
+              <span className="text-sm font-medium text-gray-700">S/.</span>
+            </Checkbox>
+
+            <Input
+              type="text"
+              value={montoDescuento}
+              label="Aplicar descuento"
+              labelPlacement="outside"
+              placeholder="0.00"
+              isDisabled={!descuentoActivado}
+              onChange={(e) => {
+                const { value } = e.target;
+                if (/^\d*\.?\d{0,2}$/.test(value)) {
+                  setMontoDescuento(value);
+                } else if (value === '' || value === '.') {
+                  setMontoDescuento(value);
+                }
+              }}
+              onKeyDown={validateDecimalInput}
+              className="w-[8.5rem]"
+              classNames={{
+                inputWrapper: descuentoActivado
+                  ? "bg-white"
+                  : "bg-gray-100",
+                input: "text-base text-gray-700",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+
+        <div className="flex mb-4">
+            <div>
+            {/* Cambio */}
+            <Input
+                label="Cambio"
+                labelPlacement="outside"
+                placeholder="S/."
+                value={cambio >= 0 ? cambio.toFixed(2) : ""}
+                readOnly
+                className="input-c w-40 ml-2"
+                style={{
+                height: "40px",
+                border: "1px solid #ddd",
+                borderRadius: "8px",
+                padding: "0 12px",
+                fontSize: "16px",
+                color: "#333",
+                outline: "none",
+                transition: "border-color 0.3s ease, box-shadow 0.3s ease",
+                }}
+            />
+            </div>
+            <div className="ml-12 w-60">
+            <Input
+                label="Faltante"
+                labelPlacement="outside"
+                placeholder="S/."
+                value={faltante >= 0 ? faltante.toFixed(2) : ""}
+                readOnly
+                className="input-c w-40 ml-2"
+                style={{
+                height: "40px",
+                border: "1px solid #ddd",
+                borderRadius: "8px",
+                padding: "0 12px",
+                fontSize: "16px",
+                color: "#333",
+                outline: "none",
+                transition: "border-color 0.3s ease, box-shadow 0.3s ease",
+                }}
+            />
+            </div>
+      </div>
+    </>
+  );
+};
+
+PagoDetalles.propTypes = {
+  totalImporte: PropTypes.number.isRequired,
+  metodo_pago: PropTypes.string.isRequired,
+  setmetodo_pago: PropTypes.func.isRequired,
+  disabledKeys1: PropTypes.array.isRequired,
+  options: PropTypes.array.isRequired,
+  montoRecibido: PropTypes.string.isRequired,
+  setMontoRecibido: PropTypes.func.isRequired,
+  validateDecimalInput: PropTypes.func.isRequired,
+  descuentoActivado: PropTypes.bool.isRequired,
+  setDescuentoActivado: PropTypes.func.isRequired,
+  montoDescuento: PropTypes.string.isRequired,
+  setMontoDescuento: PropTypes.func.isRequired,
+  cambio: PropTypes.number.isRequired,
+  faltante: PropTypes.number.isRequired,
+};
+
+export default PagoDetalles;
---
+++ client/src/pages/Ventas/Registro_Venta/ComponentsRegistroVentas/Modals/useCobrarModalState.jsx
@@ -0,0 +1,135 @@
+import { useState, useEffect } from 'react';
+import useClientesData from '../../../Data/data_cliente_venta';
+import useProductosData from '../../../Data/data_producto_venta';
+import useSucursalData from '../../../Data/data_sucursal_venta';
+import { useLastData } from '../../../Data/getLastVenta';
+import generateComprobanteNumber from '../../../Data/generate_comprobante';
+
+const useCobrarModalState = () => {
+  const { productos } = useProductosData();
+  const { sucursales } = useSucursalData();
+  const { last } = useLastData();
+  const { clientes, addCliente } = useClientesData();
+
+  const [montoRecibido, setMontoRecibido] = useState('');
+  const [observacion, setObservacion] = useState('');
+  const [descuentoActivado, setDescuentoActivado] = useState(false);
+  const [montoDescuento, setMontoDescuento] = useState(0);
+  const [montoRecibido2, setMontoRecibido2] = useState('');
+  const [comprobante_pago, setcomprobante_pago] = useState('Boleta');
+  const [metodo_pago, setmetodo_pago] = useState('');
+  const [metodo_pago2, setmetodo_pago2] = useState('');
+  const [montoRecibido3, setMontoRecibido3] = useState('');
+  const [metodo_pago3, setmetodo_pago3] = useState('');
+  const [showConfirmacion, setShowConfirmacion] = useState(false);
+  const [showNuevoCliente, setShowNuevoCliente] = useState(false);
+  const [tipo_cliente, settipo_cliente] = useState('Natural');
+  const [serie, SetSerie] = useState('');
+  const [nu, SetNum] = useState('');
+  const [clienteSeleccionado, setClienteSeleccionado] = useState('');
+  const [dniOrRuc, setDni] = useState('');
+  const [nombreCliente, setNombreCliente] = useState('');
+  const [direccionCliente, setDireccionCliente] = useState('');
+
+  const loadDetallesFromLocalStorage = () => {
+    const savedDetalles = localStorage.getItem('detalles');
+    return savedDetalles ? JSON.parse(savedDetalles) : [];
+  };
+  const detalles = loadDetallesFromLocalStorage();
+
+  const options = [
+    { key: 'EFECTIVO', value: 'EFECTIVO', label: 'EFECTIVO' },
+    { key: 'PLIN', value: 'PLIN', label: 'PLIN' },
+    { key: 'YAPE', value: 'YAPE', label: 'YAPE' },
+    { key: 'VISA', value: 'VISA', label: 'VISA' },
+    { key: 'AMERICAN EXPRESS', value: 'AMERICAN EXPRESS', label: 'AMERICAN EXPRESS' },
+    { key: 'DEPOSITO BBVA', value: 'DEPOSITO BBVA', label: 'DEPOSITO BBVA' },
+    { key: 'DEPOSITO BCP', value: 'DEPOSITO BCP', label: 'DEPOSITO BCP' },
+    { key: 'DEPOSITO CAJA PIURA', value: 'DEPOSITO CAJA PIURA', label: 'DEPOSITO CAJA PIURA' },
+    { key: 'DEPOSITO INTERBANK', value: 'DEPOSITO INTERBANK', label: 'DEPOSITO INTERBANK' },
+    { key: 'MASTER CARD', value: 'MASTER CARD', label: 'MASTER CARD' },
+  ];
+
+  const disabledKeys1 = comprobante_pago !== 'Nota de venta'
+    ? options.filter(({ value }) => value === metodo_pago2 || value === metodo_pago3).map(({ key }) => key)
+    : options.filter(({ value }) => value !== 'EFECTIVO').map(({ key }) => key);
+
+  const disabledKeys2 = options.filter(({ value }) => value === metodo_pago || value === metodo_pago3).map(({ key }) => key);
+  const disabledKeys3 = options.filter(({ value }) => value === metodo_pago || value === metodo_pago2).map(({ key }) => key);
+
+  const fetchComprobanteNumber = async () => {
+    try {
+      const comprobante_pago = JSON.parse(localStorage.getItem('comprobante')) || {};
+      const comp = comprobante_pago.comprobante_pago;
+
+      if (!comp) {
+        console.warn('El valor de comp no es válido:', comp);
+        return;
+      }
+
+      const nuevoNumComprobante = await generateComprobanteNumber(comp);
+      SetSerie(nuevoNumComprobante.substring(1, nuevoNumComprobante.indexOf('-')));
+      SetNum(nuevoNumComprobante.substring(nuevoNumComprobante.indexOf('-') + 1));
+      localStorage.setItem('comprobante1', JSON.stringify({ nuevoNumComprobante }));
+    } catch (error) {
+      console.error('Error al obtener el número de comprobante:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchComprobanteNumber();
+  }, [comprobante_pago]);
+
+  return {
+    productos,
+    sucursales,
+    last,
+    clientes,
+    addCliente,
+    montoRecibido,
+    setMontoRecibido,
+    observacion,
+    setObservacion,
+    descuentoActivado,
+    setDescuentoActivado,
+    montoDescuento,
+    setMontoDescuento,
+    montoRecibido2,
+    setMontoRecibido2,
+    comprobante_pago,
+    setcomprobante_pago,
+    metodo_pago,
+    setmetodo_pago,
+    metodo_pago2,
+    setmetodo_pago2,
+    montoRecibido3,
+    setMontoRecibido3,
+    metodo_pago3,
+    setmetodo_pago3,
+    showConfirmacion,
+    setShowConfirmacion,
+    showNuevoCliente,
+    setShowNuevoCliente,
+    tipo_cliente,
+    settipo_cliente,
+    serie,
+    SetSerie,
+    nu,
+    SetNum,
+    clienteSeleccionado,
+    setClienteSeleccionado,
+    dniOrRuc,
+    setDni,
+    nombreCliente,
+    setNombreCliente,
+    direccionCliente,
+    setDireccionCliente,
+    detalles,
+    options,
+    disabledKeys1,
+    disabledKeys2,
+    disabledKeys3,
+  };
+};
+
+export default useCobrarModalState;
