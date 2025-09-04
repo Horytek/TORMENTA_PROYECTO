ce0155dc96133e4a2838fc950abb10f0e75f9781
Davist Edu Bustamante <bustamante777a@gmail.com>
2025-09-04 08:33:50 -0500

    Go_13_Azure

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ import { config } from "dotenv";
 
 config();
 
-export const HOST = process.env.HOST || "";
-export const DATABASE = process.env.DATABASE || "";
-export const USER = process.env.USERNAME || "";
-export const PASSWORD = process.env.PASSWORD || "";
-export const PORT_DB = process.env.PORT_DB || "3306";
+export const HOST = process.env.DB_HOST || "";
+export const DATABASE = process.env.DB_DATABASE || "";
+export const USER = process.env.DB_USERNAME || "";
+export const PASSWORD = process.env.DB_PASSWORD || "";
+export const PORT_DB = process.env.DB_PORT || "3306";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:4000";
