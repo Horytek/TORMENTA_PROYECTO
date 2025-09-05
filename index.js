5a1a5d12e8e3430c18320e448d8717598ead30c0
Davist Edu Bustamante <bustamante777a@gmail.com>
2025-09-04 22:46:08 -0500

    Let's_Go

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import app from './src/app.js';
 
-const port = process.env.PORT || 8080;
-
 const main = () => {
     let server;
     
     try {
-        server = app.listen(port, '0.0.0.0', () => {
-            console.log(`Servidor escuchando en el puerto ${port}`);
+        server = app.listen(app.get('port'), '0.0.0.0', () => {
+            //console.log(`✅ Servidor corriendo en http://0.0.0.0:${app.get('port')}`);
+            //console.log(`🌍 Entorno: ${process.env.NODE_ENV || 'development'}`);
         });
 
         server.on('error', (err) => {
             if (err.code === 'EADDRINUSE') {
-                console.error(`❌ El puerto ${port} ya está en uso. Intenta con otro puerto o libera el actual.`);
+                console.error(`❌ El puerto ${app.get('port')} ya está en uso. Intenta con otro puerto o libera el actual.`);
             } else {
                 console.error('❌ Error al iniciar el servidor:', err);
             }
@@ -28,7 +27,9 @@ const main = () => {
         });
         
         function gracefulShutdown() {
+            //console.log('🛑 Cerrando servidor...');
             server.close(() => {
+                //console.log('✅ Servidor cerrado correctamente');
                 process.exit(0);
             });
             
