8ceb014b89c5ae067eb339be3ab3cee6dc0d2e60
Davist Edu Bustamante <Davist_Bustamante@sternenzelt.net.pe>
2025-10-08 17:33:31 -0500

    Prueba_v6

--- src/database/database.js
+++ src/database/database.js
@@ -17,7 +17,7 @@ const pool = mysql.createPool({
 const getConnection = async () => {
     try {
         const connection = await pool.getConnection();
-        //console.log("Connected to the database");
+        console.log("Connected to the database");
         return connection;
     } catch (error) {
         console.error("Error connecting to the database:", error);
